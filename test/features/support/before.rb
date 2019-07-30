c73ce06bbc87e98ae5c5b823113740c388065bf0
Shamim Ahmed <shamim@amazon-ca61ab25a.localdomain>
2019-07-30 12:02:05 -0400

    updated feature scenario and config files

--- test/features/support/before.rb
+++ test/features/support/before.rb
@@ -3,7 +3,7 @@ Before do |scenario|
   unless $dunit
     Environment.setTier ENV['TIER']
     p "Tier set to: #{ENV['TIER']}"
-    $driver = UI_Helper_Methods.getDriver (ENV["Browser"])
+    #$driver = UI_Helper_Methods.getDriver (ENV["Browser"])
     $dunit = true
   end
 end
