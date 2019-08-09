8d37a90ed768c009e5ce84fffed246050e2164cd
Sreeharsha Chennuru <schennuru@sevatec.com>
2019-08-09 12:32:32 -0400

    Updated the ruby image name

--- master-pod-template.groovy
+++ master-pod-template.groovy
@@ -115,7 +115,7 @@ spec:
       - name: docker-sock-volume
         mountPath: /var/run/docker.sock
   - name: ruby
-    image: circleci/ruby:latest
+    image: ruby
     imagePullPolicy: Always
     command:
     - cat
